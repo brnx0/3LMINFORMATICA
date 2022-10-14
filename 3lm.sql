954631e6339c0a7420fc5a09b3b5807e9c5476b6
Breno Oliveira <90010580+brnx0@users.noreply.github.com>
2022-10-14 19:41:27 -0300

    Update 3lm.sql

--- 3lm.sql
+++ 3lm.sql
@@ -2,15 +2,17 @@
 --
 -- Banco de dados: `3lm`
 CREATE DATABASE IF NOT EXISTS 3lm;
+USE 3lm;
 -- -------------------------------------------------------
 --
 -- Estrutura da tabela `grupos`
 --
 
 CREATE TABLE `grupos` (
-  `id` int(11) NOT NULL,
-  `grupo` varchar(25) NOT NULL
+  `id` int(11) PRIMARY KEY auto_increment,
+  `grupo` varchar(25) NOT NULL UNIQUE KEY
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 -- --------------------------------------------------------
 
@@ -19,59 +21,16 @@ CREATE TABLE `grupos` (
 --
 
 CREATE TABLE `produtos` (
-  `idProduto` int(11) NOT NULL,
-  `descricao` varchar(50) NOT NULL,
-  `codBarra` int(11) NOT NULL,
+  `idProduto` int(11) PRIMARY KEY auto_increment,
+  `descricao` varchar(50) NOT NULL UNIQUE KEY,
+  `codBarra` int(11) NOT NULL UNIQUE KEY,
   `saldo` float NOT NULL,
   `undMedida` varchar(5) NOT NULL,
   `tipo` varchar(5) NOT NULL,
   `grupo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `grupos`
---
-ALTER TABLE `grupos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `grupo` (`grupo`);
-
---
--- Índices para tabela `produtos`
---
-ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`idProduto`),
-  ADD UNIQUE KEY `codBarra` (`codBarra`),
-  ADD UNIQUE KEY `descricao` (`descricao`),
-  ADD KEY `fk_idGrupo` (`grupo`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `grupos`
---
-ALTER TABLE `grupos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, 
-
---
--- AUTO_INCREMENT de tabela `produtos`
---
-ALTER TABLE `produtos`
-  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT, 
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `produtos`
---
 ALTER TABLE `produtos`
   ADD CONSTRAINT `fk_idGrupo` FOREIGN KEY (`grupo`) REFERENCES `grupos` (`id`);
-COMMIT;
+
 
