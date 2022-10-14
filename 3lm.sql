36b5d5ec6770ec07dc53bfd51e539020ab8062d5
Breno Oliveira <90010580+brnx0@users.noreply.github.com>
2022-10-14 02:47:44 -0300

    bd

---
+++ 3lm.sql
@@ -0,0 +1,77 @@
+
+--
+-- Banco de dados: `3lm`
+CREATE DATABASE IF NOT EXISTS 3lm;
+-- -------------------------------------------------------
+--
+-- Estrutura da tabela `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `id` int(11) NOT NULL,
+  `grupo` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `idProduto` int(11) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `codBarra` int(11) NOT NULL,
+  `saldo` float NOT NULL,
+  `undMedida` varchar(5) NOT NULL,
+  `tipo` varchar(5) NOT NULL,
+  `grupo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `grupos`
+--
+ALTER TABLE `grupos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `grupo` (`grupo`);
+
+--
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`idProduto`),
+  ADD UNIQUE KEY `codBarra` (`codBarra`),
+  ADD UNIQUE KEY `descricao` (`descricao`),
+  ADD KEY `fk_idGrupo` (`grupo`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `grupos`
+--
+ALTER TABLE `grupos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `fk_idGrupo` FOREIGN KEY (`grupo`) REFERENCES `grupos` (`id`);
+COMMIT;
+
